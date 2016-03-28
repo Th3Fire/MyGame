c37f31efe103530e4a41580ebb61a2c9e01dafb7
wuttinunt <wuttinunt.th@gmail.com>
2016-03-29 01:08:20 +0700

    add my project

---
+++ game01/core/src/main/java/sut/game01/core/MyGame.java
@@ -0,0 +1,37 @@
+package sut.game01.core;
+
+import static playn.core.PlayN.*;
+import playn.core.Game;
+import tripleplay.game.ScreenStack;
+import playn.core.util.Clock;
+
+
+public class MyGame extends Game.Default {
+
+
+    public static final int UPDATE_RATE = 25;
+    private ScreenStack ss = new ScreenStack();
+    protected  final Clock.Source clock = new Clock.Source(UPDATE_RATE);
+
+    public MyGame(){
+        super(UPDATE_RATE);
+    }
+  @Override
+  public void init() {
+    // create and add background image layer
+      ss.push(new HomeGame(ss));
+
+  }
+
+  @Override
+  public void update(int delta) {
+      ss.update(delta);
+  }
+
+  @Override
+  public void paint(float alpha) {
+      clock.paint(alpha);
+      ss.paint(clock);
+    // the background automatically paints itself, so no need to do anything here!
+  }
+}
